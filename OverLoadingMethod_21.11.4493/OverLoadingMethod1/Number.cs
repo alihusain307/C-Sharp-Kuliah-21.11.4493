bf45e4bd39ad46afd44e6045101326445a2ff2f8
21.11.4493 M. Ali Husain <71416659+alihusain307@users.noreply.github.com>
2022-04-19 02:32:09 +0700

    Laporan Praktikum

---
+++ OverLoadingMethod_21.11.4493/OverLoadingMethod1/Number.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloadingMethod
+{
+    class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            int minim;
+
+            if (number1 < number2)
+            {
+                minim = number1;
+            }
+            else
+            {
+                minim = number2;
+            }
+            return minim;
+        }
+
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            int minim;
+
+            if ((number1 < number2) && (number1 < number3))
+            {
+                minim = number1;
+
+            }
+            else if ((number2 < number3) && (number2 < number1))
+            {
+                minim = number2;
+            }
+            else
+            {
+                minim = (int)number3;
+            }
+            return minim;
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            int max;
+
+            if (number1 > number2)
+            {
+                max = number1;
+            }
+            else
+            {
+                max = number2;
+            }
+            return max;
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            int max;
+
+            if ((number1 > number2) && (number1 > number3))
+            {
+                max = number1;
+            }
+            else if ((number2 > number3) && (number2 > number1))
+            {
+                max = number2;
+            }
+            else
+            {
+                max = (int)number3;
+            }
+            return max;
+        }
+    }
+}
